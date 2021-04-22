7c49304f0b74e6ac265dccc70512b9882519e6de
USER <USER@121.140.166.139>
2021-04-23 03:49:23 +0900

    GreetingDTO

---
+++ udong/src/main/java/com/greeting/GreetingDTO.java
@@ -0,0 +1,62 @@
+package com.greeting;
+
+public class GreetingDTO {
+	int num, listNum;
+	String userId;
+	String nickName;
+	String subject;
+	String content;
+	String created;
+	int hitCount;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	
+	
+}
---
+++ udong/src/main/java/com/used/UsedDTO.java
@@ -0,0 +1,17 @@
+package com.used;
+
+public class UsedDTO {
+	int num, listNum;
+	String userId;
+	String nickName;
+	String subject;
+	String content;
+	String imageFilename;
+	String area;
+	String price;
+	String created;
+	String cateNum;
+	int hitCount;
+	
+	
+}
