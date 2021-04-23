46bf5920d29a85f97e845497ffc0b3743a7e0a67
USER <USER@121.140.166.139>
2021-04-23 18:24:02 +0900

    greeting

---
+++ udong/src/main/java/com/greeting/GreetingDAO.java
@@ -0,0 +1,532 @@
+package com.greeting;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.util.DBConn;
+
+public class GreetingDAO {
+	private Connection conn = DBConn.getConnection();
+	//DB에 INSERT 
+	public int insertGreeting(GreetingDTO dto) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "INSERT INTO greeting(num, userId, subject, content, created, hitCount)"
+				+ " VALUES(greeting_seq.NEXTVAL, ?, ?, ? SYSDATE, 0)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getUserId());
+			pstmt.setString(2, dto.getSubject());
+			pstmt.setString(3, dto.getContent());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+	
+	//전체데이터수
+	public int dataCount() { 
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT COUNT(*) FROM greeting";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs= pstmt.executeQuery(); //rs=실행결과
+			if(rs.next()) { 
+				result = rs.getInt(1); //위 SELECT 문에 컬럼명 없어서 첫번째라는 의미의 1
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+	
+	//게시물 리스트 //greeting과 member1 EQUI JOIN (member1에서 nickName을 가져와야 하기 때문)
+		public List<GreetingDTO> listGreeting(int offset, int rows) {
+			List<GreetingDTO> list = new ArrayList<>();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql;
+			
+			try {
+				sql = "SELECT num, g.userId, nickName, subject, hitCount, TO_CHAR(created, 'YYYY-MM-DD') created "
+						+ " FROM greeting g "
+						+ " JOIN member1 m ON g.userId = m.userId "
+						+ " ORDER BY num DESC "
+						+ " OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ";
+				
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, offset);
+				pstmt.setInt(2, rows);
+				
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					GreetingDTO dto = new GreetingDTO();
+					dto.setNum(rs.getInt("num"));
+					dto.setNickName(rs.getString("nickName"));
+					dto.setSubject(rs.getString("subject"));
+					dto.setHitCount(rs.getInt("hitCount"));
+					dto.setCreated(rs.getString("created"));
+					
+					list.add(dto); 				
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if(rs!=null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+					}
+				}
+				
+				if(pstmt!=null) {
+					try {
+						pstmt.close();
+					}catch (Exception e2) {					
+					}
+				}
+			}
+			return list;
+		}
+
+		//검색 전체 데이터 수 //greeting과 member1 EQUI JOIN
+			public int dataCount(String condition, String keyword) {
+				int result = 0;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				String sql;
+				
+				try {
+					sql = "SELECT COUNT(*) FROM greeting g JOIN member1 m ON g.userId = m.userId ";
+					if(condition.equals("all")) { //제목과 내용 둘 다 검색 가능
+						sql  += " WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ";
+					}else if(condition.equals("created")) {
+						keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+						sql += " WHERE TO_CHAR(created, YYYYMMDD') = ? ";
+					}else {
+						sql += " WHERE INSTR(" + condition + ", ?) >= 1";
+					}
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, keyword);
+					if(condition.equals("all")) {
+						pstmt.setString(2, keyword);
+					}
+					
+					rs = pstmt.executeQuery();
+					if(rs.next() ) {
+						result = rs.getInt(1);
+					}
+					
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if(rs!=null) {
+						try {
+							rs.close();
+						} catch (Exception e2) {
+						}
+					}
+					
+					if(pstmt!=null) {
+						try {
+							pstmt.close();
+						} catch (Exception e2) {
+						}
+					}
+				}
+				return result;
+			}
+			
+			//검색일때 리스트 //greeting과 member1 EQUI JOIN
+			public List<GreetingDTO> listBoard(int offset, int rows, String condition, String keyword) {
+				List<GreetingDTO> list = new ArrayList<GreetingDTO>();
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				String sql;
+				
+				try {
+					sql = "SELECT num, g.userId, nickName, subject, hitCount, TO_CHAR(created, 'YYYY-MM-DD') created "
+							+ " FROM greeting g"
+							+ " JOIN member1 m ON g.userId = m.userId ";
+					if(condition.equals("all")) {
+						sql += " WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ";
+					}else if(condition.equals("created")) {
+						keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+						sql += " WHERE TO_CHAR(created, 'YYYYMMDD') = ?";
+					}else {
+						sql += " WHERE INSTR(" + condition + ", ?) >=1";
+					}
+						sql	+= " ORDER BY num DESC "
+							+ " OFFSET ? ROWS FETCH FIRST ? ROWS ONLY";
+						
+						pstmt = conn.prepareStatement(sql);
+						
+					if(condition.equals("all")) {					
+						pstmt.setString(1, keyword);
+						pstmt.setString(2, keyword);
+						pstmt.setInt(3, offset);
+						pstmt.setInt(4, rows);
+					} else {
+						pstmt.setString(1, keyword);
+						pstmt.setInt(2, offset);
+						pstmt.setInt(3, rows);
+					}
+					rs = pstmt.executeQuery();
+					while(rs.next()) {
+						GreetingDTO dto = new GreetingDTO();
+						dto.setNum(rs.getInt("num"));
+						dto.setUserId(rs.getString("userId"));
+						dto.setNickName(rs.getString("nickName"));
+						dto.setSubject(rs.getString("subject"));
+						dto.setHitCount(rs.getInt("hitCount"));
+						dto.setCreated(rs.getString("created"));
+						
+						list.add(dto); 
+					}
+					
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if(rs!=null) {
+						try {
+							rs.close();
+						} catch (Exception e2) {
+						}
+					}
+					
+					if(pstmt!=null) {
+						try {
+							pstmt.close();
+						} catch (Exception e2) {
+						}
+					}
+				}
+				
+				return list;
+			}
+			
+
+			//글 조회수 증가
+			public int updateHitCount(int num) throws SQLException {
+				int result = 0;
+				PreparedStatement pstmt = null;
+				String sql;
+				
+				try {
+					sql = "UPDATE bbs SET hitCount = hitCount + 1 WHERE num = ?";
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setInt(1, num);  //해당되는 num일 경우 hitCount+1을 줘야함
+					result = pstmt.executeUpdate();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if(pstmt !=null) {
+						try {
+							pstmt.close();
+						} catch (Exception e2) {
+						}
+					}
+				}
+				return result;
+			}
+			
+			//글보기 
+			public GreetingDTO readBoard(int num) {
+				GreetingDTO dto = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				String sql;
+				
+				try {
+					sql = "SELECT num, g.userId, nickName, subject, content, hitCount, created FROM greeting g " //userId 가져와야 게시글 올린사람만 수정 및 삭제 가능
+							+ " JOIN member1 m ON g.userId = m.userId "
+							+ " WHERE num = ?";
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setInt(1,num);
+					
+					rs= pstmt.executeQuery();
+					if(rs.next()) {
+						dto = new GreetingDTO();
+						dto.setNum(rs.getInt("num"));
+						dto.setUserId(rs.getString("userId"));
+						dto.setNickName(rs.getString("nickName"));
+						dto.setSubject(rs.getString("subject"));
+						dto.setContent(rs.getString("content"));
+						dto.setHitCount(rs.getInt("hitCount"));
+						dto.setCreated(rs.getString("created"));
+					}						
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if(rs!=null) {
+						try {
+							rs.close();
+						} catch (Exception e2) {
+						}
+					}
+					if(pstmt!=null) {
+						try {
+							pstmt.close();
+						} catch (Exception e2) {
+						}
+					}
+				}
+				return dto;
+			}
+			
+			//이전글
+			public GreetingDTO preReadGreeting(int num, String condition, String keyword) {
+				GreetingDTO dto = null;
+				
+				PreparedStatement pstmt =null;
+				ResultSet rs = null;
+				StringBuilder sb = new StringBuilder();
+				
+				try {
+					if(keyword.length() != 0) {//검색
+		                sb.append("SELECT num, subject FROM greeting g JOIN member1 m ON g.userId = m.userId "); //게시물번호, 제목
+		                
+		                if(condition.equals("all")) {//제목또는 내용으로 검색
+		                    sb.append(" WHERE ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >=1 ");
+		                } else if(condition.equals("created")) { //등록일자로 검색
+		                	keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+		                    sb.append(" WHERE (TO_CHAR(created, 'YYYYMMDD') = ?) ");
+		                } else {//username, subject, contet 중 하나일 때 
+		                    sb.append(" WHERE ( INSTR("+condition+", ?) > 0) ");//제목,내용,이름 다 검색 가능
+		                }
+		                sb.append("            AND (num > ? ) "); //num이 커야함
+		                sb.append(" ORDER BY num ASC "); //큰것 중에 오름차순 정렬
+		                sb.append(" FETCH  FIRST  1  ROWS  ONLY "); //정렬한 것 중에 첫번째 데이터(제일 작은거)만 가져온다
+
+		                pstmt=conn.prepareStatement(sb.toString());
+		                
+		                if(condition.equals("all")) {
+		                    pstmt.setString(1, keyword);
+		                    pstmt.setString(2, keyword);
+		                   	pstmt.setInt(3, num);
+		                } else {
+		                    pstmt.setString(1, keyword);
+		                   	pstmt.setInt(2, num);
+		                }
+		            } else {
+		                sb.append("SELECT num, subject FROM bbs ");
+		                sb.append(" WHERE num > ? ");
+		                sb.append(" ORDER BY num ASC ");
+		                sb.append(" FETCH  FIRST  1  ROWS  ONLY ");
+
+		                pstmt=conn.prepareStatement(sb.toString());
+		                pstmt.setInt(1, num);
+		            }
+		        	
+		            rs=pstmt.executeQuery();
+
+		            if(rs.next()) {
+		                dto=new GreetingDTO();
+		                dto.setNum(rs.getInt("num"));
+		                dto.setSubject(rs.getString("subject"));
+		            }
+
+		            if(rs.next()) {
+		                dto=new GreetingDTO();
+		                dto.setNum(rs.getInt("num"));
+		                dto.setSubject(rs.getString("subject"));
+		            }
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					if(rs!=null) {
+						try {
+							rs.close();
+						} catch (Exception e) {
+						}
+					}
+					if(pstmt!=null) {
+						try {
+							pstmt.close();
+						} catch (Exception e) {
+						}
+					}
+				}
+				return dto;
+				
+		}	
+				
+	    		
+		//다음글 
+		public GreetingDTO nextReadGreeting(int num, String condition, String keyword) {
+	      GreetingDTO dto=null;
+
+		  PreparedStatement pstmt=null;
+		  ResultSet rs=null;
+		  StringBuilder sb = new StringBuilder();
+		       
+		  try {
+			  if(keyword.length() != 0) {
+				  sb.append("SELECT num, subject FROM greeting g ");
+				  sb.append(" JOIN member1 m ON  g.userId = m.userId ");
+				  if(condition.equals("all")) {
+					  sb.append(" WHERE ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1  )  ");
+				  } else if(condition.equals("created")) {
+					  keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+					  sb.append(" WHERE (TO_CHAR(created, 'YYYYMMDD') = ?) ");
+				  } else {
+					  sb.append(" WHERE ( INSTR("+condition+", ?) > 0) ");
+				  }
+				  sb.append("          AND (num < ? ) ");
+				  sb.append(" ORDER BY num DESC ");
+				  sb.append(" FETCH  FIRST  1  ROWS  ONLY ");
+
+				  pstmt=conn.prepareStatement(sb.toString());
+				  if(condition.equals("all")) {
+					  pstmt.setString(1, keyword);
+					  pstmt.setString(2, keyword);
+					  pstmt.setInt(3, num);
+				  } else {
+					  pstmt.setString(1, keyword);
+					  pstmt.setInt(2, num);
+				  }
+			  } else {
+				  sb.append("SELECT num, subject FROM bbs ");
+				  sb.append(" WHERE num < ? ");
+				  sb.append(" ORDER BY num DESC ");
+				  sb.append(" FETCH  FIRST  1  ROWS  ONLY ");
+
+				  pstmt=conn.prepareStatement(sb.toString());
+				  pstmt.setInt(1, num);
+			  }
+
+			  rs=pstmt.executeQuery();
+
+			  if(rs.next()) {
+				  dto=new GreetingDTO();
+				  dto.setNum(rs.getInt("num"));
+				  dto.setSubject(rs.getString("subject"));
+			  }
+		  } catch (SQLException e) {
+			  e.printStackTrace();
+		  } finally {
+			  if(rs!=null) {
+				  try	{
+					  rs.close();
+				  }catch (SQLException e2){
+				  }
+			  }
+			  if(pstmt!=null) {
+				  try	{
+					  pstmt.close();
+				  }catch (SQLException e2){
+				  }
+			  }
+		  }
+
+		  return dto;
+		}
+			
+		//수정
+		public int updateBoard(GreetingDTO dto) throws SQLException {
+			int result = 0;
+			PreparedStatement pstmt = null;
+			String sql;
+				
+			try {
+				sql= "UPDATE greeting SET subject=?, content=? WHERE num=? AND userId=?";//and조건->작성한 사람만 수정 가능
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(1, dto.getSubject());
+				pstmt.setString(2, dto.getContent());
+				pstmt.setInt(3, dto.getNum());
+				pstmt.setString(4, dto.getUserId());
+					
+				result = pstmt.executeUpdate();//쿼리실행
+									
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			} finally {
+				if(pstmt!=null) {
+					try {
+						pstmt.close();
+					} catch (Exception e2) {
+					}
+				}
+			}
+				
+				
+			return result;
+		}
+			
+		//삭제 //게시글 올린사람 or 관리자만 삭제 가능 
+		public int deleteGreeting(int num, String userId) throws SQLException {
+			int result = 0;
+			PreparedStatement pstmt =null;
+			String sql;
+				
+			try {
+				sql="DELETE FROM greeting WHERE num = ?";
+				if( ! userId.equals("admin")) { //관리자가 아닌경우 
+					sql += " AND userId = ?"; //작성한 사람만 삭제 가능 
+				}
+				pstmt = conn.prepareStatement(sql);				
+				pstmt.setInt(1, num);
+				if(! userId.equals("admin")) {
+					pstmt.setString(2, userId);
+				}			
+				result = pstmt.executeUpdate();						
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			} finally {
+				if(pstmt !=null) {
+					try {
+						pstmt.close();
+					} catch (Exception e2) {
+					}
+				}
+			}
+			return result;
+		}
+			
+			
+					
+	}
---
+++ udong/src/main/java/com/greeting/GreetingServlet.java
@@ -0,0 +1,205 @@
+package com.greeting;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import com.member.SessionInfo;
+import com.util.MyUtil;
+
+@WebServlet("/greeting/*")
+public class GreetingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	protected void forward(HttpServletRequest req, HttpServletResponse resp, String path) throws ServletException, IOException {
+		//포워딩
+		RequestDispatcher rd = req.getRequestDispatcher(path);
+		rd.forward(req, resp);
+	}
+	
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		
+		String uri = req.getRequestURI();
+		
+		//로그인이 되지 않은 상태이면 로그인 페이지로
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		if (info==null) { //=로그인된 정보가 없다 => 로그인되어있지 x -> 로그인 페이지로
+			String cp = req.getContextPath();
+			resp.sendRedirect(cp+"/member/login.do");
+			return;
+		}
+		
+		if(uri.indexOf("list.do")!=-1) {//리스트
+			list(req, resp);
+		} else if (uri.indexOf("created.do") != -1) {//글작성
+			created(req, resp);
+		} else if(uri.indexOf("created_ok.do")!=-1) {//글보내기
+			createdSubmit(req, resp);
+		}else if(uri.indexOf("article.do")!=-1) {//글보기
+			article(req, resp);
+		}else if(uri.indexOf("update.do")!=-1) {//수정
+			updateForm(req, resp);
+		}else if(uri.indexOf("update_ok.do")!=-1) {//수정완료
+			updateSubmit(req, resp);
+		}else if(uri.indexOf("delete.do")!=-1) {//삭제
+			delete(req, resp);
+		}	
+	}
+	
+	//리스트
+	protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {		
+		GreetingDAO dao = new GreetingDAO();
+		MyUtil util = new MyUtil();
+		String cp = req.getContextPath();		
+		
+		//파라미터로 넘어온 페이지 번호
+		String page = req.getParameter("page");
+		int current_page = 1;
+		if(page !=null) {
+			current_page = Integer.parseInt(page);
+		}
+		//검색
+		String condition = req.getParameter("condition");
+		String keyword = req.getParameter("keyword");
+		if(condition == null) {
+			condition = "all";
+			keyword="";
+		}
+		
+		//GET 방식일 경우 디코딩
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			keyword = URLDecoder.decode(keyword, "utf-8");
+		}
+		
+		//전체 데이터 개수
+		int dataCount;
+		if(keyword.length()==0) {
+			dataCount = dao.dataCount();
+		} else {
+			dataCount = dao.dataCount(condition, keyword);
+		}
+		//전체 페이지 수 
+		int rows = 10; 
+		int total_page = util.pageCount(rows, dataCount);
+		if(current_page> total_page) {
+			current_page = total_page;
+		}
+		int offset = (current_page -1) * rows;
+		if(offset < 0) offset = 0;
+		
+		//게시글 가져오기
+		List<GreetingDTO> list = null;
+		if(keyword.length()==0) {
+			list = dao.listGreeting(offset, rows);
+		}else {
+			list = dao.listBoard(offset, rows, condition, keyword);
+		}		
+		
+		//리스트 글번호 만들기
+		int listNum, n=0;
+		for(GreetingDTO dto : list) {
+			listNum = dataCount - (offset+n);
+			dto.setListNum(listNum);
+			n++;
+		}
+		
+		String query="";
+		if(keyword.length()!=0) {
+			query="condition="+condition+"&keyword="+URLEncoder.encode(keyword, "utf-8");
+		}
+		
+		//페이징 처리
+		String listUrl = cp+"/greeting/list.do";
+		String articleUrl = cp+"/greeting/article.do?page="+current_page;
+		if(query.length()!=0) {
+			listUrl += "?" +query;
+			articleUrl += "&" +query;
+		}
+		String paging= util.paging(current_page, total_page, listUrl);
+		
+		//포워딩할 JSP로 넘길 속성
+		req.setAttribute("list", list);
+		req.setAttribute("paging",paging);
+		req.setAttribute("page", current_page );
+		req.setAttribute("dataCount", dataCount );
+		req.setAttribute("total_page", dataCount );
+		req.setAttribute("articleUrl", articleUrl );
+		req.setAttribute("condition", condition);
+		req.setAttribute("keyword", keyword );
+		
+		//JSP로 포워딩
+		forward(req, resp, "/WEB-INF/views/bbs/list.jsp");
+	}
+	
+	//글작성
+	protected void created(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("mode", "created");
+		forward(req, resp, "/WEB-INF/views/greeting/created.jsp");
+	}
+	
+	//글저장
+	protected void createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		GreetingDAO dao = new GreetingDAO();
+
+		try {
+			GreetingDTO dto=new GreetingDTO();
+			
+			// userId는 세션에 저장된 정보
+			dto.setUserId(info.getUserId());
+			
+			// 파라미터
+			dto.setSubject(req.getParameter("subject"));
+			dto.setContent(req.getParameter("content"));
+			
+			dao.insertGreeting(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		resp.sendRedirect(cp+"/greeting/list.do");
+	}
+	
+	//글보기
+	protected void updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	//글수정
+	protected void updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	//글수정 완료
+	protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	//글 삭제
+	protected void article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+}
