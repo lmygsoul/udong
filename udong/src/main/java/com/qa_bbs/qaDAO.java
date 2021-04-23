682bb25ceae3cf422d86d071119f93e8598384ad
lmygs <lmygs@DESKTOP-63DCS8O>
2021-04-23 14:34:53 +0900

    qa, class 추가 -MY

---
+++ udong/src/main/java/com/qa_bbs/qaDAO.java
@@ -0,0 +1,5 @@
+package com.qa_bbs;
+
+public class qaDAO {
+	
+}
---
+++ udong/src/main/java/com/qa_bbs/qaDTO.java
@@ -0,0 +1,79 @@
+package com.qa_bbs;
+
+public class qaDTO {
+	int boradNum;
+	String userId;
+	String subject;
+	String content;
+	int groupNum;
+	int dept;
+	int orderNo;
+	int parent;
+	int hitCount;
+	String created;
+	
+	
+	public int getBoradNum() {
+		return boradNum;
+	}
+	public void setBoradNum(int boradNum) {
+		this.boradNum = boradNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getGroupNum() {
+		return groupNum;
+	}
+	public void setGroupNum(int groupNum) {
+		this.groupNum = groupNum;
+	}
+	public int getDept() {
+		return dept;
+	}
+	public void setDept(int dept) {
+		this.dept = dept;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	
+	
+}
