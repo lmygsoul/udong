bc3d42ed8cadc486677b8b0b2854dd5eefbf2ef7
Jaeromi <Jaeromi@118.37.192.223>
2021-04-29 19:07:39 +0900

    neighborDAO_수정

--- udong/src/main/java/com/neighbor/NeighborDAO.java
+++ udong/src/main/java/com/neighbor/NeighborDAO.java
@@ -419,4 +419,203 @@ private Connection conn = DBConn.getConnection();
 		
 		return result;
 	}
+	
+	public int dataCount_nb(String userId) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			sql = "SELECT NVL(COUNT(*), 0) FROM neighbor_bbs WHERE userId = ?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+	
+	public int dataCount_nb(String condition, String keyword, String userId) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			sql = "SELECT NVL(COUNT(*), 0) FROM neighbor_bbs b LEFT OUTER JOIN member1 m  ON b.userid=m.userid";
+			if(condition.equals("all")) {
+				sql+=" WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 AND b.userId = ? ";
+			} else if(condition.equals("created")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sql += " WHERE TO_CHAR(created, 'YYYYMMDD') = ? AND b.userId = ? ";
+			} else {
+				sql += " WHERE INSTR(" +condition+", ?) >=1 AND b.userId = ? ";
+			}
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, keyword);
+			if(condition.equals("all")) {
+				pstmt.setString(2, keyword);
+				pstmt.setString(3, userId);
+			} else {
+				pstmt.setString(2, userId);
+			}
+			
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				result=rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				} 
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+
+	public List<NeighborDTO> listBoard_nb(int offset, int rows,String userId) {
+		List<NeighborDTO> list = new ArrayList<NeighborDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			sql=" SELECT b.num, nickName, subject, TO_CHAR(created,'YYYY-MM-DD') created, hitCount, rec, notRec  "
+					+ " FROM neighbor_bbs b"
+					+ " LEFT OUTER JOIN member1 m ON b.userId = m.userId"
+					+ " LEFT OUTER JOIN (SELECT num, COUNT(CASE WHEN type=1 then 1 end) rec, COUNT(CASE WHEN type=2 then 1 end) notRec"
+					+ " FROM neighbor_rec GROUP BY num) r ON b.num = r.num"
+					+ " WHERE b.userId = ?"
+					+ " ORDER BY num DESC"
+					+ " OFFSET ? ROWS FETCH FIRST ? ROW ONLY";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.setInt(2, offset);
+			pstmt.setInt(3, rows);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				NeighborDTO dto = new NeighborDTO();
+				dto.setNum(rs.getInt("num"));
+				dto.setNickName(rs.getString("nickname"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setCreated(rs.getString("created"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setRec(rs.getInt("rec"));
+				dto.setNotRec(rs.getInt("notRec"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				} 
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return list;
+	}
+
+	public List<NeighborDTO> listBoard_nb(int offset, int rows, String condition, String keyword,String userId) {
+		List<NeighborDTO> list = new ArrayList<NeighborDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			sql=" SELECT b.num, nickName, subject, TO_CHAR(created,'YYYY-MM-DD') created, hitCount, rec, notRec  "
+					+ " FROM neighbor_bbs b"
+					+ " LEFT OUTER JOIN member1 m ON b.userId = m.userId"
+					+ " LEFT OUTER JOIN (SELECT num, COUNT(CASE WHEN type=1 then 1 end) rec, COUNT(CASE WHEN type=2 then 1 end) notRec"
+					+ " FROM neighbor_rec GROUP BY num) r ON b.num = r.num";
+				
+			if(condition.equals("created")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sql += " WHERE TO_CHAR(created, 'YYYYMMDD') = ? AND b.userId = ? ";
+			} else if(condition.equals("all")) {
+				sql += " WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 AND b.userId = ? ";
+			} else {
+				sql += " WHERE INSTR("+condition+", ?) >= 1 AND b.userId = ? ";
+			}
+					
+			sql	+= " ORDER BY num DESC;"
+				+ " OFFSET ? ROWS FETCH FIRST ? ROW ONLY";
+			pstmt = conn.prepareStatement(sql);
+            if(condition.equals("all")) {
+    			pstmt.setString(1, keyword);
+    			pstmt.setString(2, keyword);
+    			pstmt.setString(3, userId);
+    			pstmt.setInt(4, offset);
+    			pstmt.setInt(5, rows);
+            } else {
+    			pstmt.setString(1, keyword);
+    			pstmt.setString(2, userId);
+    			pstmt.setInt(3, offset);
+    			pstmt.setInt(4, rows);
+            }
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				NeighborDTO dto = new NeighborDTO();
+				dto.setNum(rs.getInt("num"));
+				dto.setNickName(rs.getString("nickname"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setCreated(rs.getString("created"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setRec(rs.getInt("rec"));
+				dto.setNotRec(rs.getInt("notRec"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				} 
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return list;
+	}
 }
+
